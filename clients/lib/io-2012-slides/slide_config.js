9101830e39702c622fc87197ad3d60a2b87942dd
emepyc <emepyc@gmail.com>
2013-04-13 21:17:32 +0100

    client libraries

---
+++ clients/lib/io-2012-slides/slide_config.js
@@ -0,0 +1,36 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Introducing <span style="color:white">e!Peek</span>',
+    subtitle: 'The minimal genome browser',
+    //eventTitle: 'Google I/O 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Miguel Pignatelli',
+    company: 'Software developer, Ensembl Compara - EBI',
+    gplus: 'https://plus.google.com/111225444277358003374',
+    www: 'http://www.ebi.ac.uk/~mp/',
+    github: 'http://github.com/emepyc/'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
