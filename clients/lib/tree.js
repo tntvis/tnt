a6eef77bd60c6d1e31534247029861462153ec59
emepyc <emepyc@gmail.com>
2013-06-23 23:32:28 +0100

    New client tree.html -- wip

---
+++ clients/lib/tree.js
@@ -0,0 +1,27 @@
+var species_tree = {
+    "name": "root",
+    "children": [
+	{
+	    "name": "N1",
+	    "children": [
+		{
+		    "name": "N2",
+		    "children": [
+			{"name": "Species1"},
+			{"name": "Species2"},
+			{"name": "Species3"}
+		    ]
+		},
+		{
+		    "name": "N3",
+		    "children": [
+			{"name": "Species4"},
+			{"name": "Species5"}
+		    ]
+		}
+	    ]
+	}
+    ]
+};
+
+
