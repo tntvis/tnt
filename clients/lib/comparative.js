432c700328482cc3bfe7e76d48549735192e22fe
emepyc <emepyc@gmail.com>
2013-10-27 16:59:03 +0000

    Genome browsers now go on a table with species icons

--- clients/lib/comparative.js
+++ clients/lib/comparative.js
@@ -21,20 +21,43 @@ var epeek_theme = function() {
     "use strict";
 
     var pathToScript = scriptPath('comparative.js');
-    console.log("PATH_TO_SCRIPT:" + pathToScript);
+
+    var species_to_icon_filename = {
+	'human' : "Homo_sapiens.png",
+	'mouse' : "Mus_musculus.png",
+    };
 
     // Now, gBs is an array of gBs
     var gBrowserTheme = function(gBs, div) {
 
+	var table = d3.select(div)
+	    .append("table")
+// 	    .style("border", "1px solid gray");
+	    .attr("border", "1px solid gray")
+	    .attr("margin", "0px")
+// 	    .style("border-collapse", "collapse")
+	    .style("border-spacing", "0px");
+
+	var caption_row = table
+	    .append("tr")
+
+	caption_row
+	    .append("td")
+	    .append("p")
+	    .text("Species")
+
+	var control_td = caption_row
+	    .append("td")
+
 	// The controls pane
 	// TODO: The style elements should be included in a CSS file should we have a separate stylesheet for this theme
-	var control_pane = d3.select(div)
+//	var control_pane = d3.select(div)
+	var control_pane = control_td
 	    .append("div")
 	    .attr("class", "ePeek_control_pane")
 	    .style("margin-left", "auto")
 	    .style("margin-right", "auto")
-	    .style("width", "30%")
-	    .style("margin-bottom", "20px");
+	    .style("width", "30%");
 
 	var left_button = control_pane
 	    .append("button")
@@ -94,11 +117,23 @@ var epeek_theme = function() {
 	    setTimeout( function() {
 		var gB = gBs[i];
 
-		// We need a div per genomeBrowser
-		var thisDiv = d3.select(div).append("div").attr("id","genomeBrowser" + i);
-		gB(thisDiv[0][0]);
+		var table_row = table
+		    .append("tr");
 
-		var gDiv = d3.select("#genomeBrowser" + i + " .ePeek_groupDiv");
+		table_row
+		    .append("td")
+		    .append("img")
+		    .attr("src", pathToScript + "../../themes/pics/" + species_to_icon_filename[gB.species()]);
+
+		table_row
+		    .append("td")
+		    .append("div")
+		    .attr("id", "ePeek_comparative" + i);
+
+		gB(document.getElementById("ePeek_comparative" + i));
+
+
+		var gDiv = d3.select("#ePeek_comparative" + i + " .ePeek_groupDiv");
 
 		// left chevron
 		gDiv
@@ -113,6 +148,11 @@ var epeek_theme = function() {
 		    .on("mouseout", function(){
 			d3.select(this).attr("src", pathToScript + "../chevron_inactive_left.png")
 		    });
+
+
+		// The locRow needs to give some room on the left
+		d3.selectAll(".ePeek_locRow")
+		    .style("margin-left", "25px");
 
 		// right chevron
 		gDiv
