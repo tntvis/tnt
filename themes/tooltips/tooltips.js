dfa89b385e7dd3932137a924f75c496c480abd4c
emepyc <emepyc@gmail.com>
2013-12-23 00:49:26 +0000

    Reorganization of lib: genome namespace created

--- themes/tooltips/tooltips.js
+++ themes/tooltips/tooltips.js
@@ -2,7 +2,7 @@ var epeek_theme = function() {
     "use strict";
 
     var theme = function(gB, div) {
-	gB.gene_info_callback = epeek.tooltip()
+	gB.gene_info_callback = epeek.genome.tooltip()
 	    .background_color(gB.background_color())
 	    .foreground_color(gB.foreground_color()); //gene_info_callback;
 	
