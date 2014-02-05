87a90ce75cf853dfce16a2a63c5029bb98848028
Miguel Pignatelli <mp@ebi.ac.uk>
2014-02-05 18:37:03 +0000

    Implemented transition (still buggy)

--- themes/species_tree/species_tree.js
+++ themes/species_tree/species_tree.js
@@ -3,7 +3,13 @@ var epeek_theme = function() {
     "use strict";
 
     var tree_theme = function (sT, div) {
+	sT.layout("vertical");
 	sT(div);
+	sT.transition();
+	setTimeout(function(){
+	    sT.layout("radial");
+	    sT.transition();
+	}, 2000);
     };
 
     return tree_theme;
