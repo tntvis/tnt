6b5426e3c33ffd9e527bf5a0606a5d288e8a74eb
Miguel Pignatelli <mp@ebi.ac.uk>
2014-02-07 00:29:46 +0000

    Working with transitions

--- themes/species_tree/species_tree.js
+++ themes/species_tree/species_tree.js
@@ -3,12 +3,12 @@ var epeek_theme = function() {
     "use strict";
 
     var tree_theme = function (sT, div) {
-	sT.layout("vertical");
-	sT(div);
-	sT.transition();
+	sT.layout("radial");
+	var t = sT(div);
+	console.log(t.find_node_by_name(t, "Homo_sapiens"));
 	setTimeout(function(){
-	    sT.layout("radial");
-	    sT.transition();
+	    sT.layout("vertical");
+	    sT.update();
 	}, 2000);
     };
 
