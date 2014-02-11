7d5f4c4f567bb175532ec5a67ef038de0d0b7059
Miguel Pignatelli <mp@ebi.ac.uk>
2014-02-11 21:04:35 +0000

    wip: removed nodes/links

--- themes/species_tree/species_tree.js
+++ themes/species_tree/species_tree.js
@@ -3,11 +3,11 @@ var epeek_theme = function() {
     "use strict";
 
     var tree_theme = function (sT, div) {
-	sT.layout("radial");
+	sT.layout("vertical");
 	sT(div);
 	setTimeout(function(){
 // 	    sT.layout("vertical");
-	    sT.subtree(["Mus_musculus", "Bos_taurus"])
+	    sT.subtree(["Mus_musculus", "Bos_taurus", "Homo_sapiens"])
  	    sT.update();
 	}, 2000);
     };
