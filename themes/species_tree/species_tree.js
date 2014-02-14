815aebd1da5417fe3b0f1f4729db274670bc2315
Miguel Pignatelli <mp@ebi.ac.uk>
2014-02-14 20:33:03 +0000

    wip pull_children

--- themes/species_tree/species_tree.js
+++ themes/species_tree/species_tree.js
@@ -7,7 +7,7 @@ var epeek_theme = function() {
 	sT(div);
 	setTimeout(function(){
 // 	    sT.layout("vertical");
-	    sT.subtree(["human", "mouse", "other2"]);
+	    sT.subtree(["human", "mouse", "Papio_hamadryas", "o2"]);
 	    // sT.subtree(["Homo_sapiens", "Mus_musculus", "Gorilla_gorilla"]);
  	    sT.update();
 	}, 2000);
