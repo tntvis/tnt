2d493d595be8fa0e9e4c3765b362cd18abfb6a6d
emepyc <emepyc@gmail.com>
2014-02-16 19:05:36 +0000

    inSubTree is now an object

--- themes/species_tree/species_tree.js
+++ themes/species_tree/species_tree.js
@@ -3,14 +3,21 @@ var epeek_theme = function() {
     "use strict";
 
     var tree_theme = function (sT, div) {
-	sT.layout("radial");
+	sT.layout("vertical");
 	sT(div);
 	setTimeout(function(){
+	    console.log("GO");
 // 	    sT.layout("vertical");
-	    sT.subtree(["human", "mouse", "Papio_hamadryas", "o2"]);
-	    // sT.subtree(["Homo_sapiens", "Mus_musculus", "Gorilla_gorilla"]);
+	    sT.subtree(["kk1", "chimp", "human", "mouse", "?Papio_hamadryas", "o2"]);
+//	    sT.subtree(["Homo_sapiens", "Mus_musculus", "Gorilla_gorilla"]);
+//	    sT.subtree(["human","fish"]);
  	    sT.update();
 	}, 2000);
+// 	setTimeout(function(){
+// 	    console.log("GO");
+// 	    sT.subtree(["human", "mouse", "chimp", "fish"]);
+// 	    sT.update();
+// 	}, 7000);
     };
 
     return tree_theme;
