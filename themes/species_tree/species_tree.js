5c7df0f6839bd4777da157a11be5a2bd6b8ea318
emepyc <emepyc@gmail.com>
2014-02-17 09:01:02 +0000

    wip -- pull_parent

--- themes/species_tree/species_tree.js
+++ themes/species_tree/species_tree.js
@@ -8,16 +8,16 @@ var epeek_theme = function() {
 	setTimeout(function(){
 	    console.log("GO");
 // 	    sT.layout("vertical");
-	    sT.subtree(["kk1", "chimp", "human", "mouse", "?Papio_hamadryas", "o2"]);
+	    sT.subtree(["kk1", "chimp", "human", "fish", "?Papio_hamadryas", "o2"]);
 //	    sT.subtree(["Homo_sapiens", "Mus_musculus", "Gorilla_gorilla"]);
 //	    sT.subtree(["human","fish"]);
  	    sT.update();
-	}, 2000);
-// 	setTimeout(function(){
-// 	    console.log("GO");
-// 	    sT.subtree(["human", "mouse", "chimp", "fish"]);
-// 	    sT.update();
-// 	}, 7000);
+	}, 4000);
+	setTimeout(function(){
+	    console.log("GO");
+	    sT.subtree(["human", "mouse", "chimp", "fish"]);
+	    sT.update();
+	}, 7000);
     };
 
     return tree_theme;
