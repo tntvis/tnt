b4db67dc6fd5818409836eddb03d877c77cedb47
Miguel Pignatelli <mp@ebi.ac.uk>
2014-02-18 17:53:32 +0000

    wip -- sync the expansion

--- themes/species_tree/species_tree.js
+++ themes/species_tree/species_tree.js
@@ -8,16 +8,16 @@ var epeek_theme = function() {
 	setTimeout(function(){
 	    console.log("GO");
 // 	    sT.layout("vertical");
-	    sT.subtree(["kk1", "chimp", "human", "fish", "?Papio_hamadryas", "o2"]);
+	    sT.subtree(["kk1", "chimp", "human", "fish", "?Papio_hamadryas", "o2", "Danio_rerio"]);
 //	    sT.subtree(["Homo_sapiens", "Mus_musculus", "Gorilla_gorilla"]);
 //	    sT.subtree(["human","fish"]);
  	    sT.update();
-	}, 4000);
+	}, 2000);
 	setTimeout(function(){
 	    console.log("GO");
 	    sT.subtree(["human", "mouse", "chimp", "fish"]);
 	    sT.update();
-	}, 9000);
+	}, 6000);
     };
 
     return tree_theme;
