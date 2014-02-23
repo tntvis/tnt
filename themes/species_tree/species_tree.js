ee5213c594b99ab8cff76124bc4e5d3274e8ba41
emepyc <emepyc@gmail.com>
2014-02-23 23:29:49 +0000

    subtrees working with simple examples

--- themes/species_tree/species_tree.js
+++ themes/species_tree/species_tree.js
@@ -8,16 +8,20 @@ var epeek_theme = function() {
 	setTimeout(function(){
 	    console.log("GO");
 // 	    sT.layout("vertical");
-	    sT.subtree(["kk1", "chimp", "human", "fish", "?Papio_hamadryas", "o2", "Danio_rerio"]);
+//	    sT.subtree(["chimp", "human", "fly"]);
+//	    sT.subtree(["mouse", "human", "zebrafish"]);
 //	    sT.subtree(["Homo_sapiens", "Mus_musculus", "Gorilla_gorilla"]);
-//	    sT.subtree(["human","fish"]);
+	    sT.subtree(["human","fish"]);
+//	    sT.subtree(["Microbat","Flying fox","Hedgehog","Shrew","Panda","Dog","Ferret","Cat","Cow","Pig","Alpaca","Dolphin","Horse"]);
  	    sT.update();
 	}, 2000);
 	setTimeout(function(){
 	    console.log("GO");
-	    sT.subtree(["human", "mouse", "chimp", "fish"]);
+//	    sT.subtree(["human", "mouse", "chimp", "fish", "fly"]);
+	    sT.subtree(["mouse", "human", "zebrafish", "fugu"]);
+//	    sT.subtree(["Microbat","Flying fox","Hedgehog","Shrew","Panda","Dog","Ferret","Cat","Cow","Pig","Alpaca","Dolphin","Horse","Sheep"]);
 	    sT.update();
-	}, 6000);
+	}, 8000);
     };
 
     return tree_theme;
