dc0904629bf0efa2d781a458cb04a95e5556837d
emepyc <emepyc@gmail.com>
2014-03-01 00:30:07 +0000

    Merged stashed

--- themes/species_tree/species_tree.js
+++ themes/species_tree/species_tree.js
@@ -3,7 +3,7 @@ var epeek_theme = function() {
     "use strict";
 
     var tree_theme = function (sT, div) {
-	sT.layout("vertical");
+	sT.layout("radial");
 	sT(div);
 	setTimeout(function(){
 	    console.log("GO");
@@ -23,6 +23,21 @@ var epeek_theme = function() {
 	    sT.update();
 	}, 8000);
     };
+
+// 	setTimeout(function(){
+// 	    console.log("GO");
+// // 	    sT.layout("vertical");
+// 	    sT.subtree(["kk1", "chimp", "human", "fish", "?Papio_hamadryas", "o2", "Danio_rerio"]);
+// //	    sT.subtree(["Homo_sapiens", "Mus_musculus", "Gorilla_gorilla"]);
+// //	    sT.subtree(["human","fish"]);
+//  	    sT.update();
+// 	}, 2000);
+// 	setTimeout(function(){
+// 	    console.log("GO");
+// 	    sT.subtree(["human", "mouse", "chimp", "fish"]);
+// 	    sT.update();
+// 	}, 6000);
+//     };
 
     return tree_theme;
 };
