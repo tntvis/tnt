525cffc409f5ff59abed075c8ae2ea3da83e9d22
Miguel Pignatelli <mp@ebi.ac.uk>
2014-03-05 15:30:00 +0000

    Added more tests

--- test/rest/rest.js
+++ test/rest/rest.js
@@ -13,6 +13,107 @@ describe('ePeek REST', function () {
     })
 
     describe('Data retrieval', function () {
+	// Being friendly with the REST API. The tests are delayed this time
+	var delay = 300;
+	describe('Ensembl External Ids', function () {
+	    it("Has a url.xref field", function () {
+		assert.isDefined(rest.url.xref);
+	    })
+	    var name = "BRCA2"
+	    var xref_url = rest.url.xref({species:"human", name:name});
+	    it("Has the correct url", function () {
+		assert.equal(xref_url, "http://beta.rest.ensembl.org/xrefs/symbol/human/BRCA2.json?object_type=gene");
+	    })
+	    it("Retrieves xrefs from the REST server", function (done) {
+		rest.call ({ url : xref_url,
+			     success : function (resp) {
+				 assert.isArray(resp);
+				 assert.isObject(resp[0]);
+				 _.each(resp, function (el) {
+				     assert.isObject(el);
+				     assert.property(el, "type");
+				     assert.equal(el.type, "gene");
+				     assert.property(el, "id");
+				 });
+				 setTimeout(done, delay);
+				 // done();
+			     }
+			   })
+	    })
+	    describe('Species names', function () {
+		it("Accepts scientific species names", function (done) {
+		    var species = "homo_sapiens";
+		    var name = "BRCA2";
+		    rest.call({ url : rest.url.xref({species:species, name:name}),
+				success : function (resp) {
+				    assert.isArray(resp);
+				    assert.isObject(resp[0]);
+				    setTimeout(done, delay);
+				    // done();
+				}
+			      })
+		})
+		it("Accepts scientific species names without underscores", function (done) {
+		    var species = "homo sapiens";
+		    var name = "BRCA2";
+		    rest.call({ url : rest.url.xref({species:species, name:name}),
+				success : function (resp) {
+				    assert.isArray(resp);
+				    assert.isObject(resp[0]);
+				    setTimeout(done, 400);
+				    // done();
+				}
+			      })
+		})
+		it("Accepts common names", function (done) {
+		    var species = "human";
+		    var name = "BRCA2";
+		    rest.call({ url : rest.url.xref({species:species, name:name}),
+				success : function (resp) {
+				    assert.isArray(resp);
+				    assert.isObject(resp[0]);
+				    setTimeout(done, delay);
+				    // done();
+				}
+			      })
+		})
+	    })
+	    it ("Fires the error callback on wrong url", function (done) {
+		rest.call ({url : xref_url + "xxx", /// wrong url
+			    error : function (err) {
+				assert.isDefined(err);
+				assert.equal(err.status, 400);
+				assert.equal(err.readyState, 4);
+				assert.equal(err.statusText, "Bad Request");
+				setTimeout(done, delay);
+				// done();
+			    }
+			   })
+	    })
+	});
+
+	describe('Chromosome info', function () {
+	    it("Has a url.chr_info field", function () {
+		assert.isDefined(rest.url.chr_info);
+	    })
+	    var chr_info_url = rest.url.chr_info({species:"human", chr:"13"});
+	    it ("Has the correct url", function () {
+		assert.equal(chr_info_url, "http://beta.rest.ensembl.org/assembly/info/human/13.json?format=full");
+	    })
+	    it ("Retrieves chr info", function (done) {
+		rest.call ({ url : chr_info_url,
+			     success : function (resp) {
+				 assert.isObject(resp);
+				 assert.property(resp, "is_chromosome");
+				 assert.equal(resp.is_chromosome, 1);
+				 assert.property(resp, "length");
+				 setTimeout(done, delay);
+				// done();
+			     }
+			   })
+	    })
+	})
+
 	describe('Ensembl Gene Ids', function () {
 	    it("Has a url.gene field", function () {
 		assert.isDefined(rest.url.gene)
@@ -24,14 +125,23 @@ describe('ePeek REST', function () {
 	    it("Retrieves gene from ensembl ID", function (done) {
 		rest.call ({ url : gene_url,
 			     success : function (resp) {
+				 assert.isObject(resp);
+				 assert.property(resp, "id");
 				 assert.equal(resp.id, "ENSG00000139618");
+				 assert.property(resp, "display_name");
 				 assert.equal(resp.display_name, "BRCA2");
+				 assert.property(resp, "species");
 				 assert.equal(resp.species, "homo_sapiens");
+				 assert.property(resp, "object_type");
 				 assert.equal(resp.object_type, "Gene");
+				 assert.property(resp, "biotype");
 				 assert.equal(resp.biotype, "protein_coding");
+				 assert.property(resp, "strand");
 				 assert.equal(resp.strand, 1);
+				 assert.property(resp, "seq_region_name");
 				 assert.equal(resp.seq_region_name, 13);
-				 done();
+				 setTimeout(done, delay);
+				 // done();
 			     }
 			   })
 	    })
@@ -42,10 +152,43 @@ describe('ePeek REST', function () {
 				 assert.equal(err.status, 400);
 				 assert.equal(err.readyState, 4);
 				 assert.equal(err.statusText, "Bad Request");
-				 done();
+				 setTimeout(done, delay);
+				 // done();
 			     }
 			   });
 		assert.isTrue(rest.connections() > 0);
+	    })
+	})
+
+	describe('Gene Homologues', function () {
+	    it("Has a url.homologues field", function () {
+		assert.isDefined(rest.url.homologues);
+	    })
+	    var homologues_url = rest.url.homologues({id:"ENSG00000139618"});
+	    it("Has the correct url", function () {
+		assert.equal(homologues_url, "http://beta.rest.ensembl.org/homology/id/ENSG00000139618.json?format=condensed;sequence=none;type=all");
+	    })
+	    it("Retrieves homologues", function (done) {
+		rest.call( {url : homologues_url,
+			    success : function (resp) {
+				assert.isObject(resp);
+				assert.property(resp, "data");
+				assert.isArray(resp.data);
+				assert.lengthOf(resp.data, 1);
+				assert.property(resp.data[0], "homologies");
+				assert.isArray(resp.data[0].homologies);
+				_.each(resp.data[0].homologies, function (el) {
+				    assert.match(el.type, /ortholog|paralog/);
+				});
+				var ids = _.pluck(resp.data[0].homologies, "id");
+				assert.isArray(ids),
+				assert.equal(ids.length, resp.data[0].homologies.length);
+				_.each(ids, function (el) {
+				    assert.isDefined(el);
+				})
+				setTimeout(done, delay);
+			    }
+			   })
 	    })
 	})
 
@@ -65,10 +208,14 @@ describe('ePeek REST', function () {
 		rest.call( { url : region_url,
 			     success : function (resp) {
 				 assert.isArray(resp);
-				 var ids = _.pluck(resp, 'id');
+				 var ids = _.pluck(resp, 'ID');
 				 assert.isArray(ids);
 				 assert.equal(ids.length, resp.length);
-				 done();
+				 _.each(ids, function (el) {
+				     assert.isDefined(el);
+				 })
+				 setTimeout(done, delay);
+				 // done();
 			     }
 			   });
 	    })
@@ -79,7 +226,8 @@ describe('ePeek REST', function () {
 				 assert.equal(err.status, 400);
 				 assert.equal(err.readyState, 4);
 				 assert.equal(err.statusText, "Bad Request");
-				 done();
+				 setTimeout(done, delay);
+				 // done();
 			     }
 			   });
 		assert.isTrue(rest.connections() > 0);
