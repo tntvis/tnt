f863158fa5e6e62fe70c3c638fd5ea790f078ea6
Miguel Pignatelli <mp@ebi.ac.uk>
2014-03-11 16:14:25 +0000

    Added block tracks

--- themes/minimal/minimal.js
+++ themes/minimal/minimal.js
@@ -2,33 +2,54 @@ var epeek_theme = function() {
 
     var theme = function(gB, div) {
 	gB(div);
+	// Gene Track1
 	var gene_track = epeek.genome.track.gene()
 	    .height(200)
 	    .foreground_color("red")
 	    .background_color("#cccccc");
 
+	// Gene Track2
 	var gene_track2 = epeek.genome.track.gene()
 	    .height(100)
 	    .foreground_color("blue")
 	    .background_color("#DDDD00");
 
+	// Pin Track1
 	var pin_track1 = epeek.genome.track.pin()
 	    .height(30)
 	    .pin_color("blue")
 	    .background_color("#cccccc");
 
 	var pin1_updater = pin_track1.retriever.local()
-	    .retriever(function(){return [
+	    .retriever(function () {return [
 		{
-		    pos : 32890000,
+		    pos : 32890000
+		},
+		{
+		    pos : 32896000
 		}
 	    ]});
 
 	pin_track1.update(pin1_updater);
 
+	// Block Track1
+	var block_track = epeek.genome.track.block()
+	    .height(30)
+	    .foreground_color("blue")
+	    .background_color("#FFCFDD");
+	var block_updater = block_track.retriever.local()
+	    .retriever(function () {return [
+		{
+		    start : 32890000,
+		    end   : 32890500,
+		}
+	    ]});
+	block_track.update(block_updater);
+
 	gB.add_track(gene_track);
 	gB.add_track(gene_track2);
 	gB.add_track(pin_track1);
+	gB.add_track(block_track);
 
 	gB.start();
     };
