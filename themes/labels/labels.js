d6ddd9ee0562ef40991ebbc54e00d57fa72615d9
emepyc <emepyc@gmail.com>
2014-03-18 00:54:02 +0000

    labels now includes img labels

--- themes/labels/labels.js
+++ themes/labels/labels.js
@@ -2,38 +2,11 @@ var epeek_theme = function() {
     "use strict";
 
     var tree_theme = function (sT, div) {
-	// In the div, we set up a "select" to transition between a radial and a vertical tree
-	// var menu_pane = d3.select(div)
-	//     .append("div")
-	//     .append("span")
-	//     .text("Layout:  ");
-
-	// var sel = menu_pane
-	//     .append("select")
-	//     .on("change", function(d) {
-	// 	switch (this.value) {
-	// 	case "unscaled" :
-	// 	    sT.layout().scale(false);
-	// 	    break;
-	// 	case "scaled" :
-	// 	    sT.layout().scale(true);
-	// 	    break;
-	// 	};
-	// 	sT.update();
-	//     });
-
-	// sel
-	//     .append("option")
-	//     .attr("value", "unscaled")
-	//     .attr("selected", 1)
-	//     .text("Unscaled");
-	// sel
-	//     .append("option")
-	//     .attr("value", "scaled")
-	//     .text("Scaled");
-
 
 	var newick = "(((((homo_sapiens:9,pan_troglodytes:9)207598:34,callithrix_jacchus:43)314293:52,mus_musculus:95)314146:215,taeniopygia_guttata:310)32524:107,danio_rerio:417)117571:135;"
+
+	var path = epeek.scriptPath("labels.js");
+	var pics_path = path + "/pics/";
 
 	var scientific_to_common = {
 	    "homo_sapiens" : "human",
@@ -43,13 +16,14 @@ var epeek_theme = function() {
 	    "taeniopygia_guttata" : "zebra finch",
 	    "danio_rerio" : "zebrafish"
 	};
-	var common_to_scientific = {
-	    "human" : "homo_sapiens",
-	    "chimpanzee" : "pan_troglodytes",
-	    "marmoset" : "callithrix_jacchus",
-	    "mouse" : "mus_musculus",
-	    "zebra finch" : "taeniopygia_guttata",
-	    "zebrafish" : "danio_rerio"
+
+	var names_to_pics = {
+	    "homo_sapiens" : pics_path + "homo_sapiens.png",
+	    "pan_troglodytes" : pics_path + "pan_troglodytes.png",
+	    "callithrix_jacchus" : pics_path + "callithrix_jacchus.png",
+	    "mus_musculus" : pics_path + "mus_musculus.png",
+	    "taeniopygia_guttata" : pics_path + "taeniopygia_guttata.png",
+	    "danio_rerio" : pics_path + "danio_rerio.png"
 	};
 
 	// Different labels
@@ -70,7 +44,25 @@ var epeek_theme = function() {
 	    .text(function (d) {
 		return scientific_to_common[d.name]
 	    });
+	var image_label = epeek.tree.label.img()
+	    .src(function (d) {
+		return names_to_pics[d.name];
+	    })
+	    .width(function () {
+		return 50;
+	    })
+	    .height(function () {
+		return 50;
+	    });
+	var mixed_label = function (node) {
+	    if (node.branchset !== undefined) { // internal nodew
+		original_label.call(this, node);
+	    } else { // leaf
+		image_label.call(this, node);
+	    }
+	}
 
+	// The menu to change the labels dynamically
 	var menu_pane = d3.select(div)
 	    .append("div")
 	    .append("span")
@@ -94,6 +86,13 @@ var epeek_theme = function() {
 		    break;
 		case "common" :
 		    sT.label(common_label);
+		    break;
+		case "image" :
+		    sT.label(image_label);
+		    break;
+		case "mixed" :
+		    sT.label(mixed_label);
+		    break;
 		}
 
 		sT.update();
@@ -125,6 +124,15 @@ var epeek_theme = function() {
 	    .attr("value", "common")
 	    .text("common name");
 
+	label_type_menu
+	    .append("option")
+	    .attr("value", "image")
+	    .text("species image");
+
+	label_type_menu
+	    .append("option")
+	    .attr("value", "mixed")
+	    .text("text + image");
 	sT
 	    .data(epeek.tree.parse_newick(newick))
 	    .duration(2000)
