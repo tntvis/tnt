52b4863882a3fcd2586ff64d146a371ed81876ac
emepyc <emepyc@gmail.com>
2014-03-24 00:58:02 +0000

    removed selection

--- themes/ensembl_gene_tree/ensembl_gene_tree.js
+++ themes/ensembl_gene_tree/ensembl_gene_tree.js
@@ -2,42 +2,12 @@ var epeek_theme = function() {
     "use strict";
 
     var tree_theme = function (sT, div) {
-	// In the div, we set up a "select" to transition between a radial and a vertical tree
-	var menu_pane = d3.select(div)
-	    .append("div")
-	    .append("span")
-	    .text("Layout:  ");
-
-	var sel = menu_pane
-	    .append("select")
-	    .on("change", function(d) {
-		switch (this.value) {
-		case "unscaled" :
-		    sT.layout().scale(false);
-		    break;
-		case "scaled" :
-		    sT.layout().scale(true);
-		    break;
-		};
-		sT.update();
-	    });
-
-	sel
-	    .append("option")
-	    .attr("value", "unscaled")
-	    .attr("selected", 1)
-	    .text("Unscaled");
-	sel
-	    .append("option")
-	    .attr("value", "scaled")
-	    .text("Scaled");
 
 	var label = epeek.tree.label.text()
 	    .text(function (node) {
 		if (node.children) {
 		    return "";
 		} else {
-		    console.log(node);
 		    return node.id.accession
 		}
 	    });
