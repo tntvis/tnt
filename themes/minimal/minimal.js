238038b811d30ad26b4ee0b90b6fb9ade8e64e9b
Miguel Pignatelli <mp@ebi.ac.uk>
2014-04-01 11:29:59 +0100

    genome moved to track (not all the themes updated)

--- themes/minimal/minimal.js
+++ themes/minimal/minimal.js
@@ -3,25 +3,25 @@ var epeek_theme_track_minimal = function() {
     var theme = function(gB, div) {
 	gB(div);
 	// Gene Track1
-	var gene_track = epeek.genome.track.gene()
+	var gene_track = epeek.track.track.gene()
 	    .height(200)
 	    .foreground_color("red")
 	    .background_color("#cccccc")
-	    .plotter(epeek.genome.feature.gene());
+	    .plotter(epeek.track.feature.gene());
 
 	// Gene Track2
-	var gene_track2 = epeek.genome.track.gene()
+	var gene_track2 = epeek.track.track.gene()
 	    .height(100)
 	    .foreground_color("blue")
 	    .background_color("#DDDD00")
-	    .plotter(epeek.genome.feature.gene());
+	    .plotter(epeek.track.feature.gene());
 
 	// Pin Track1
-	var pin_track1 = epeek.genome.track.pin()
+	var pin_track1 = epeek.track.track.pin()
 	    .height(30)
 	    .pin_color("blue")
 	    .background_color("#cccccc")
-	    .plotter(epeek.genome.feature.pin());
+	    .plotter(epeek.track.feature.pin());
 
 	var pin1_updater = pin_track1.retriever.local()
 	    .retriever(function () {return [
@@ -35,11 +35,11 @@ var epeek_theme_track_minimal = function() {
 	pin_track1.update(pin1_updater);
 
 	// Block Track1
-	var block_track = epeek.genome.track.block()
+	var block_track = epeek.track.track.block()
 	    .height(30)
 	    .foreground_color("blue")
 	    .background_color("#FFCFDD")
-	    .plotter(epeek.genome.feature.block());
+	    .plotter(epeek.track.feature.block());
 	var block_updater = block_track.retriever.local()
 	    .retriever(function () {return [
 		{
