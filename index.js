b8905e874c6e3772bf843f8812227dd90852f8b2
Miguel Pignatelli <emepyc@gmail.com>
2018-04-14 13:09:19 +0100

    possible fix in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var tnt;
+// var tnt;
 if (typeof tnt === "undefined") {
-    module.exports = tnt = require("./src/ta.js");
+    tnt = require("./src/ta.js");
 }
 var eventsystem = require ("biojs-events");
 eventsystem.mixin (tnt);
@@ -10,3 +10,5 @@ tnt.tree.node = require ("tnt.tree.node");
 tnt.tree.parse_newick = require("tnt.newick").parse_newick;
 tnt.tree.parse_nhx = require("tnt.newick").parse_nhx;
 tnt.board = require ("tnt.board");
+
+module.exports = tnt;
